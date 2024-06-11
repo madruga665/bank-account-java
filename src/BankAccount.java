bdf35ec38d32a02ccf92b1c5d3aabd87c87dac19
Luciano Amâncio <madruga665@hotmail.com>
2024-06-11 20:51:35 -0300

    criação da conta bancaria

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,5 +1,30 @@
+import java.text.DecimalFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class BankAccount {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        DecimalFormat decimalFormat = new DecimalFormat("#.00");
+
+        System.out.println("Por favor, digite o número da sua conta!");
+        int accountNumber = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da Agência!");
+        String bankAgency = scanner.next();
+
+        System.out.println("Por favor, digite o seu nome!");
+        String customerName = scanner.next();
+
+        System.out.println("Por favor, digite o seu saldo!");
+        double accountBalance = scanner.nextDouble();
+
+        System.out.println("Olá ".concat(customerName)
+                + ", obrigado por criar uma conta em nosso banco, sua agência é: ".concat(bankAgency)
+                + ", conta: ".concat(String.valueOf(accountNumber))
+                + " e seu saldo R$".concat(String.valueOf(decimalFormat.format(accountBalance)))
+                + " já está disponível para saque");
+
+        scanner.close();
     }
 }
